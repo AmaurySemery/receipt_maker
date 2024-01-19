5ccdb6a506bde4e431e3dbd487e6963ff6c47594
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-19 15:36:34 +0100

    Configuration des routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css'
-import NavBar from './components/NavBar'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import Estimate from './pages/Estimate';
+import Receipt from './pages/Receipt';
+import Pricing from './pages/Pricing';
 
 function App() {
 
   return (
       <div className="App">
         <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new-estimate" element={<Estimate />} />
+          <Route path="/new-receipt" element={<Receipt />} />
+          <Route path="/pricing" element={<Pricing />} />
+        </Routes>
       </div>
   )
 }
