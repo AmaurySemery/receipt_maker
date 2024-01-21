5f8feb33fd5c73ebe2c9c4af6b8d1e7eca0b4134
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-21 17:56:23 +0100

    Rendu de chaque ligne de devis

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,40 @@
-import { Estimate } from "../App"
+import { Estimate } from "../App";
 
 type cardProps = {
-    data: Estimate
-}
+  data: Estimate;
+};
 
 export default function Card({ data }: cardProps) {
-    return <>
-    <h3>{data.title}</h3>
-    <div>TODO - render the rest</div>
+  function calculateSubtotal(quantity: number, unitPrice: number) {
+    return (quantity * unitPrice).toFixed(2);
+  }
+  return (
+    <>
+      <h3>{data.title}</h3>
+      <div>
+        <div>
+          <div>Reference</div>
+          <div>Description</div>
+          <div>Quantity</div>
+          <div>Unit Price</div>
+          <div>Deposit</div>
+          <div>VAT</div>
+          <div>Sub-Total</div>
+        </div>
+        {data.tasks.map((t) => {
+          return (
+            <div key={t.reference}>
+              <div>{t.reference}</div>
+              <div>{t.description}</div>
+              <div>{t.quantity}</div>
+              <div>{t.unitPrice}</div>
+              <div>{t.deposit}</div>
+              <div>{t.vat}%</div>
+              <div>€{calculateSubtotal(t.quantity, t.unitPrice)}</div>
+            </div>
+          );
+        })}
+      </div>
     </>
+  );
 }
