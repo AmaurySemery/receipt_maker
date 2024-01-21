8d0ed7b577f0b2aee7526fd507f4629f93bca01f
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-21 18:56:41 +0100

    Imbriquer une CSS Grid dans une CSS Grid pour afficher les totaux

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,46 +24,44 @@ export default function Card({ data }: cardProps) {
 
   return (
     <>
-    <div className={styles.card}>
-      <h3>{data.title}</h3>
-      <div className={styles.container}>
-        <div className={styles.estimateheader}>
-          <div>Reference</div>
-          <div>Description</div>
-          <div>Quantity</div>
-          <div>Unit Price</div>
-          <div>Deposit</div>
-          <div>VAT</div>
-          <div>Sub-Total</div>
-        </div>
-        {data.tasks.map((t) => {
-          return (
-            <div key={t.reference} className={styles.details}>
-              <div>{t.reference}</div>
-              <div>{t.description}</div>
-              <div>{t.quantity}</div>
-              <div>{t.unitPrice}</div>
-              <div>{t.deposit}</div>
-              <div>{t.vat}%</div>
-              <div>€{calculateSubtotal(t.quantity, t.unitPrice)}</div>
-            </div>
-          );
-        })}
-        <div>
+      <div className={styles.card}>
+        <h3>{data.title}</h3>
+        <div className={styles.container}>
+          <div className={styles.estimateheader}>
+            <div>Reference</div>
+            <div>Description</div>
+            <div>Quantity</div>
+            <div>Unit Price</div>
+            <div>Deposit</div>
+            <div>VAT</div>
+            <div>Sub-Total</div>
+          </div>
+          {data.tasks.map((t) => {
+            return (
+              <div key={t.reference} className={styles.details}>
+                <div>{t.reference}</div>
+                <div>{t.description}</div>
+                <div>{t.quantity}</div>
+                <div>{t.unitPrice}</div>
+                <div>{t.deposit}</div>
+                <div>{t.vat}%</div>
+                <div>€{calculateSubtotal(t.quantity, t.unitPrice)}</div>
+              </div>
+            );
+          })}
+          <div>
             <div className={styles.total}>
-                <div>Total (without taxes)</div>
-                <div>€ {calculateTotalWithoutTaxes().toFixed(2)}</div>
+              <div>Total (without taxes)</div>
+              <div>€ {calculateTotalWithoutTaxes().toFixed(2)}</div>
+              <div>Total taxes</div>
+              <div>€ {calculateTaxes().toFixed(2)}</div>
+              <div>Total taxes included</div>
+              <div className={styles.totalwithtaxes}>
+                € {(calculateTotalWithoutTaxes() + calculateTaxes()).toFixed(2)}
+              </div>
             </div>
-            <div>Total taxes</div>
-            <div>€{ calculateTaxes().toFixed(2) }</div>
-            <div>
-                <div>
-                    Total taxes included
-                </div>
-                <div>€ { (calculateTotalWithoutTaxes() + calculateTaxes()).toFixed(2)}</div>
-            </div>
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
